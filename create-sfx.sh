2f38e8a9b2b103b418aad0e2d057025637d5d367
🌌 <Time.Rain.Star.Sky@Gmail.com>
2026-02-15 15:42:14 +0800

    同步更新

--- create-sfx.sh
+++ create-sfx.sh
@@ -7,23 +7,24 @@ OUTPUT="$(realpath "$2")"
 cd "$(dirname "$0")"
 
 # Download and extract https://github.com/mcmilk/7-Zip-zstd
-CHECKSUM="d140094f0277b49a4e895159bd734da03cd2b60fb73a65e4151edfedc612981e"
+URL="https://github.com/mcmilk/7-Zip-zstd/releases/download"
+CHECKSUM="5a55a8b6abb09d4b5ddd64c95f22a5270cb46752fb7a5353073508922b141596"
 NAME="7z25.01-zstd-x64"
 mkdir -p _cache
 BASE="_cache/$NAME"
 if [ ! -f "$BASE.exe" ]; then
-  curl --fail -L "https://github.com/mcmilk/7-Zip-zstd/releases/download/v25.01-v1.5.7-R1/$NAME.exe" -o "$BASE.exe"
+  curl --fail -L "$URL/v25.01-v1.5.7-R4/$NAME.exe" -o "$BASE.exe"
 fi
 echo "$CHECKSUM $BASE.exe" | sha256sum --quiet --check
 7z e -o"$BASE" "_cache/$NAME.exe"
 mv -f "$BASE"/7z.{exe,dll} .
 rm -rf "$BASE"
 
-CHECKSUM="cbc3babd589d971e45971d787ff100be8aaa5eab15b2694497ec3e447009e1f2"
-NAME="lzma2501"
+CHECKSUM="b3a748d6863d5f34a927d1a2bc562afc916780498cc8cbe559848b8bd1087e28"
+NAME="Sfx-Setup-x64"
 BASE="_cache/$NAME"
 if [ ! -f "$BASE.7z" ]; then
-  curl --fail -L "https://www.7-zip.org/a/$NAME.7z" -o "$BASE.7z"
+  curl --fail -L "$URL/v25.01-v1.5.7-R3/$NAME.7z" -o "$BASE.7z"
 fi
 echo "$CHECKSUM $BASE.7z" | sha256sum --quiet --check
 7z x -o"$BASE" "$BASE.7z"
@@ -31,15 +32,15 @@ echo "$CHECKSUM $BASE.7z" | sha256sum --quiet --check
 # Create SFX installer
 TEMP="$OUTPUT.payload"
 rm -rf "$TEMP"
-"$BASE/bin/7zr.exe" a "$TEMP" -ms1T -mx9 install.ps1 7z.{exe,dll}
+"./7z.exe" a "$TEMP" -ms1T -mx9 install.ps1 7z.{exe,dll}
 mv -f "$INPUT" Yunzai.tar.zst
-"$BASE/bin/7zr.exe" a "$TEMP" -mx0 Yunzai.tar.zst
+"./7z.exe" a "$TEMP" -mx0 Yunzai.tar.zst
 mv -f Yunzai.tar.zst "$INPUT"
-"$BASE/bin/7zr.exe" t "$TEMP"
-cat "$BASE/bin/7zSD.sfx" - "$TEMP" > "$OUTPUT" << 'EOF'
+"./7z.exe" t "$TEMP"
+cat "$BASE/7zSD.sfx" - "$TEMP" > "$OUTPUT" << 'EOF'
 ;!@Install@!UTF-8!
 ExecuteFile="powershell.exe"
 ExecuteParameters="-ExecutionPolicy Bypass .\install.ps1"
 ;!@InstallEnd@!
 EOF
-rm -rf 7z.exe Yunzai.7z "$TEMP" "$BASE"
+rm -rf 7z.{exe,dll} Yunzai.7z "$TEMP" "$BASE"
